a358fb5c4eb4c8e0f13133c01778bcb6cc94df1e
rodma <rodma@Update>
2015-10-24 13:24:04 -0500

    primero

---
+++ src/newpythonproject.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "rodma"
+__date__ = "$24-oct-2015 13:21:09$"
+
+if __name__ == "__main__":
+    print "Hello mundo"
